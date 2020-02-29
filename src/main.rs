16391195b693ace3ff05232e3b01020b0f5c93f2
Max Gonzih <gonzih@gmail.com>
2020-02-29 12:10:47 -0500

    Dynamically parse port on heroku

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 #![feature(async_closure)]
 
 // use serde_derive::{Deserialize, Serialize};
+use std::env;
+use std::net::{IpAddr, Ipv4Addr, SocketAddr};
 use warp::Filter;
 
 mod render;
@@ -21,11 +23,20 @@ async fn render_handler(org: String, repo: String) -> Result<impl warp::Reply, w
     Ok(warp::reply::html(page))
 }
 
+fn get_server_port() -> u16 {
+    env::var("PORT")
+        .ok()
+        .and_then(|s| s.parse().ok())
+        .unwrap_or(3030)
+}
+
 #[tokio::main]
 async fn main() {
     let render = warp::path!(String / String).and_then(render_handler);
 
     println!("Up and running, biatch");
 
-    warp::serve(render).run(([127, 0, 0, 1], 3030)).await;
+    let port = get_server_port();
+    let addr = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(0, 0, 0, 0)), port);
+    warp::serve(render).run(addr).await;
 }
